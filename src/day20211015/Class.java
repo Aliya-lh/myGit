f42442b5c946e4ccb75bac66fbae0c908966090d
LiuHuan <1576695530@qq.com>
2021-10-16 22:31:08 +0800

    类和对象修改

--- src/day20211015/Class.java
+++ src/day20211015/Class.java
@@ -28,6 +28,34 @@ class Person{
     public static void func1(){
         System.out.println("static::func1()");
     }
+
+
+    //构造方法：方法名和类名相同，且没有返回值
+    public Person(){//此处的访问修饰限定符可写可不写
+       this("caocao",18);//this调用自己的构造方法，但是只能在构造方法里面写
+        //并且只能调用一个（一次）  并且只能写在第一行
+        System.out.println("Person<init>");
+    }
+//什么时候调用哪个方法，取决于new一个对象时有没有给定参数
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+        System.out.println("Person<String,int>");
+    }
+
+
+
+    //实例代码块
+    {
+        this.name = "zhangfei";
+        System.out.println("实例代码块...");
+    }
+
+
+    static{//静态代码块
+        //this.name = "caocao";会报错，静态的方法和成员不能访问非静态的数据成员
+        System.out.println("静态代码块...");
+    }
 }
 
 
@@ -38,6 +66,8 @@ public class Class {
         //类比int a = 10;
         //如何访问对象当中的实例成员变量
         //通过对象的引用
+        Person person1 = new Person("liming",19);//带有参数的调用
+
         System.out.println(person.name);
         System.out.println(person.age);
         System.out.println(person.ch);
