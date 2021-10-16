ab43b89aa56786fcdce17457edd41e0a4fb04314
LiuHuan <1576695530@qq.com>
2021-10-16 22:31:29 +0800

    类和对象修改

---
+++ src/day20211016/List.java
@@ -0,0 +1,9 @@
+package day20211016;
+
+public class List{
+    public static void main(String[] args) {
+        MyArrayList myArrayList1 = new MyArrayList();
+        MyArrayList myArrayList2 = new MyArrayList();
+    }
+
+}
---
+++ src/day20211016/MyArrayList.java
@@ -0,0 +1,35 @@
+package day20211016;
+
+public class MyArrayList {
+    //顺序表
+    private int[] elem;//数组
+    private int usedSize;//有效的数据个数
+    private static final int intCapacity = 10;//初始容量 一般不可以修改，可以用final修饰
+
+    public MyArrayList() {
+        this.elem = new int[intCapacity];//初始化elem
+        this.usedSize = 0;//实例成员变量 可以不初始化 默认为 0
+
+
+
+
+
+
+
+
+//        public void display(){
+//            //打印顺序表
+//        }
+//        public void add(int pos,int data){
+//            //在pos位置新增元素
+//        }
+//        public boolean contains(int toFind){
+//            return true;
+//            //判断是否包含某个元素
+//        }
+//        public int search(int toFind){
+//            return true;
+     //   //查找某个元素对应的位置
+//        }
+    }
+}
