2ddb149430d4ab9d1fe4cd53b5ea218325053907
LiuHuan <1576695530@qq.com>
2021-10-24 22:29:08 +0800

    多态2

--- src/day20211010/Test.java
+++ src/day20211010/Test.java
@@ -73,7 +73,7 @@ public class Test {
 
 
 
-        public static void main(String[] args){
+        public static void main3(String[] args){
             //输入一个整数序列，判断是否是有序序列，有序，指序列中的整数从小到大排序或者从大到小排序(相同元素也视为有序)。
             //第一行输入一个整数N(3≤N≤50)。
             //第二行输入N个整数，用空格分隔N个整数。
@@ -103,23 +103,38 @@ public class Test {
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            int k=++i;
+            while(i<5){
+                System.out.print(i);
+                break;
+            }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
