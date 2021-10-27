5a9db6717a2033bbe351b07a69d5a465573adc17
LiuHuan <1576695530@qq.com>
2021-10-27 22:28:43 +0800

    继承类

---
+++ src/day20211027/TestDemo.java
@@ -0,0 +1,32 @@
+package day20211027;
+
+//import day20211025.Cycle;
+
+//抽象类
+abstract class Shape{
+    private int age;
+    public int a;
+    public abstract void draw();
+}
+
+//abstract class Cycle extends Shape{
+//
+//}
+//class A extends Cycle{
+//    @Override
+//    public void draw() {
+//
+//    }
+//}
+
+//class Cycle extends Shape{
+//    @Override
+//    public void draw() {
+//
+//    }
+//}
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
