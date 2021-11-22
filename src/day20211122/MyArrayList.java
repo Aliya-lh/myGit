60c09830fe72877d0ffc12c4748d7e60267d4ba9
LiuHuan <1576695530@qq.com>
2021-11-22 22:38:56 +0800

    顺序表 上

---
+++ src/day20211122/MyArrayList.java
@@ -0,0 +1,28 @@
+package day20211122;
+
+import java.lang.management.MemoryType;
+
+//顺序表
+public class MyArrayList {
+
+    public int[] elem;//数组
+    public int usedSize;//有效的数据个数
+    public static final int intCapacity = 10;//初始容量
+
+    public MyArrayList(){
+        this.elem = new int[intCapacity];
+        this.usedSize = 0;
+    }
+
+    //打印顺序表
+    public void display(){
+
+    }
+    //在pos位置插入一个元素
+
+
+    public static void main(String[] args) {
+        MyArrayList myArrayList = new MyArrayList();
+        MyArrayList myArrayList1 = new MyArrayList();
+    }
+}
