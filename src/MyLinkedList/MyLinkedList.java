b650ad21148723d9f69ca4436901010d210a6eb4
LiuHuan <1576695530@qq.com>
2021-11-28 22:04:57 +0800

    单链表

--- src/MyLinkedList/MyLinkedList.java
+++ src/MyLinkedList/MyLinkedList.java
@@ -1,18 +1,19 @@
 package MyLinkedList;
 
-
+//数据结构的题必须会！！一个方法至少写5遍！！每写一个题都要画图！！！
 class Node{
     public int data;//0
     public Node next;//null
 
     public Node(int data){
-    this.data = data;
-    this.next = null;//可以不写 默认为空
+        this.data = data;
+        this.next = null;
     }
-
 }
-public class MyLinkedList {
-    public Node head;//保存单链表头节点的引用
+
+public class MyLinkedList {//以不带头节点的单向非循环列表演示：
+    public Node head;//head是个引用 保存单链表头节点的引用
+    //head 是整个单链表的头 是MyLinkedList的成员属性
 
     //头插法
     public void addFirst(int data){
@@ -23,20 +24,94 @@ public class MyLinkedList {
             this.head = node;
             return;
         }
-        node.next = this.head;
-        this.head = node;
+        //单链表插入的时候要先绑后面！！
+        node.next = this.head;//这两行代码不可以换位置！！！
+        this.head = node;//这两行代码不可以换位置！！！
     }
+
+
+
+
     //尾插法
     public void addLast(int data){
+        Node node = new Node(data);
+
+        if(this.head == null){
+            //第一次插入节点
+            this.head = node;
+            return;
+        }
+
+        Node cur = this.head;
+        while(cur.next!=null){
+            cur = cur.next;
+        }//跳出循环说明cur.next为空了
+        cur.next = node;
+    }
+
+
+
+    //任意位置插入，第一个数据节点为0号下标
+    public boolean addIndex(int index,int data) {
+        if (index == 0) {
+
+        }
+        if (index == this.size()) {
+
+        }
+        //先找到 index位置的前一个节点的地址
+        searchIndex(index);
+        //进行插入
 
     }
-    //任意位置插入，第一个数据节点为0号下标
-    public boolean addIndex(int index,int data){
-        return false;
+
+    private Node searchIndex(int index){
+        //1、对index进行合法性检查
+        if(index<0||index>this.size()){
+            throw new RuntimeException("index位置不合法！");
+        }
+        Node cur = this.head;//index-1
     }
-    //查找是否包含关键字key是否在单链表中
+
+
+
+
+    //查找是否包含关键字key 包含返回true 不包含返回false
     public boolean contains(int key){
-        return false;
+       Node cur = this.head;
+       while(cur!=null){
+            if(cur.data==key){
+                return true;
+            }
+            cur = cur.next;
+       }
+       return false;
+    }
+
+
+
+
+    //得到单链表的长度
+    public int  size(){
+        int count = 0;
+        Node cur = this.head;
+        while(cur!=null){
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+
+
+
+    //打印单链表
+    public void display() {
+        Node cur = this.head;
+        //一般情况下不让head轻易改变，所以定义一个cur
+        while(cur!=null){
+            System.out.println(cur.data +" ");
+            cur = cur.next;
+        }
     }
 
 }
---
+++ src/MyLinkedList/TestDemo.java
@@ -0,0 +1,21 @@
+package MyLinkedList;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addFirst(10);
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+        myLinkedList.display();//头插法 打印出来数字是逆序的
+        System.out.println(myLinkedList.size());
+//        System.out.println("=================");
+//        myLinkedList.addLast(77);
+//        myLinkedList.addLast(78);
+//        myLinkedList.addLast(79);
+//        myLinkedList.addLast(80);
+//        myLinkedList.display();//尾插法 打印出来数字是正序的
+
+
+    }
+}
